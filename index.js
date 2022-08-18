06dd706efef687146981cd4c2e5a2a1cd196c419
Daniel Rienas <daniel.rienas@dieschneidergruppe.de>
2022-08-18 15:09:11 +0200

    add basic auth

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+const API_USER = process.env.API_USER || null;
+if (!API_USER) {
+  console.log(`No API_USER found`);
+  process.exit(0);
+}
+
+const API_PW = process.env.API_PW || null;
+if (!API_PW) {
+  console.log(`No JWT API_PW found`);
+  process.exit(0);
+}
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -33,7 +45,6 @@ if (!ELASTIC_URL) {
 }
 
 const ElasticIndex = `${ELASTIC_URL}/acrm_custsync/_search`;
-// const ElasticIndex = `${ELASTIC_URL}/customers/_search`;
 
 const app = express();
 
@@ -59,8 +70,32 @@ app.use((req, res, next) => {
     return;
   }
 
-  const token = req.headers.authorization.split(' ')[1];
-  const { valid, data } = verifyToken(token);
+  let tsplit = req.headers.authorization.split(' ');
+
+  if (tsplit.length < 2) {
+    res.status(401).send({
+      status: 'UNAUTHORIZED',
+      message: 'Invalid token',
+    });
+    return;
+  }
+
+  let valid, data;
+  const token = tsplit[1];
+
+  switch (tsplit[0]) {
+    case 'Basic':
+      const nToken = Buffer.from(`${API_USER}:${API_PW}`).toString('base64');
+      data = null;
+      valid = nToken === token;
+      break;
+    case 'Bearer':
+    default:
+      let v = verifyToken(token);
+      valid = v.valid;
+      data = v.data;
+      break;
+  }
 
   if (!valid) {
     res.status(401).send({
